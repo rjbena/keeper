2b179d1e81e15d856bf3f8e98342b8a00df2a9de
ricky <rjbenavides@cpp.edu>
2020-11-09 09:13:33 -0800

    Part 1 of Keeper App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
